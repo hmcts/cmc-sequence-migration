3a0287cef6818c03b3a0657048f4b7ffe6003db7
Karim Lawani <k7lawani@gmail.com>
2019-08-14 18:49:53 +0100

    enable

--- migration.sh
+++ migration.sh
@@ -42,59 +42,6 @@ echo "* NEW DB password: $NEW_PASSWORD"
 echo "* NEW DB name: $NEW_DB"
 echo
 
-OLD_CONN_STR="host=$OLD_HOST port=$OLD_PORT dbname=$OLD_DB user=$OLD_USERNAME password=$OLD_PASSWORD sslmode=require"
-NEW_CONN_STR="host=$NEW_HOST port=$NEW_PORT dbname=$NEW_DB user=$NEW_USERNAME password=$NEW_PASSWORD sslmode=require"
-
-LAST_VAL_SQL="SELECT last_value from claim_reference_number_seq"
-LAST_VAL_LEGAL_SQL="SELECT last_value from claim_legal_rep_reference_number_seq"
-
-if [ "$REFERENCETYPE" == "legal" ]; then
-    LAST_VAL_SQL=$LAST_VAL_LEGAL_SQL
-fi
-
-LAST_VAL=$(psql "${OLD_CONN_STR}" \
-    -t \
-    -c "${LAST_VAL_SQL}" \
-    --set ON_ERROR_ROLLBACK=on \
-    --set ON_ERROR_STOP=off
-    )
-
-echo "LAST SEQUENCE VALUE FROM OLD DB: $LAST_VAL"
-
-SEQ_VAL_BEFORE_UPDATE=$(psql "${NEW_CONN_STR}" \
-    -t \
-    -c "${LAST_VAL_SQL}" \
-    --set ON_ERROR_ROLLBACK=on \
-    --set ON_ERROR_STOP=off
-    )
-
-echo "SEQUENCE VALUE IN NEW DB BEFORE UPDATE: $SEQ_VAL_BEFORE_UPDATE"
-
-UPDATE_VAL_SQL="SELECT setval('claim_reference_number_seq', $LAST_VAL)"
-UPDATE_LEGAL_SQL="SELECT setval('claim_legal_rep_reference_number_seq', $LAST_VAL)"
-
-if [ "$REFERENCETYPE" == "legal" ]; then
-    UPDATE_VAL_SQL=$UPDATE_LEGAL_SQL
-fi
-
-UPDATE_NEW_DB=$(psql "${NEW_CONN_STR}" \
-    -t \
-    -c "${UPDATE_VAL_SQL}" \
-    --set ON_ERROR_ROLLBACK=on \
-    --set ON_ERROR_STOP=off
-    )
-
-echo "UPDATE SEQUENCE VALUE ON NEW DB: $UPDATE_NEW_DB"
-
-SEQ_VAL_AFTER_UPDATE=$(psql "${NEW_CONN_STR}" \
-    -t \
-    -c "${LAST_VAL_SQL}" \
-    --set ON_ERROR_ROLLBACK=on \
-    --set ON_ERROR_STOP=off
-    )
-
-echo "SEQUENCE VALUE IN NEW DB AFTER UPDATE: $SEQ_VAL_AFTER_UPDATE"
-
 echo "* Migration finished"
 echo
 exit 0
