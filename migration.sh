95aa77b6df43a2c56696946fe31ae6dbcf69464b
Karim Lawani <k7lawani@gmail.com>
2019-08-16 14:00:07 +0200

    trying

--- migration.sh
+++ migration.sh
@@ -42,6 +42,7 @@ echo "* NEW DB name: $NEW_DB"
 echo
 
 OLD_CONN_STR="host=$OLD_HOST port=$OLD_PORT dbname=$OLD_DB user=$OLD_USERNAME password=$OLD_PASSWORD sslmode=require"
+NEW_CONN_STR="host=$NEW_HOST port=$NEW_PORT dbname=$NEW_DB user=$NEW_USERNAME password=$NEW_PASSWORD sslmode=require"
 
 LAST_VAL_SQL="SELECT last_value from claim_reference_number_seq"
 
@@ -53,3 +54,12 @@ LAST_VAL=$(psql "${OLD_CONN_STR}" \
     )
 
 echo "LAST SEQUENCE VALUE FROM OLD DB: $LAST_VAL"
+
+SEQ_VAL_BEFORE_UPDATE=$(psql "${NEW_CONN_STR}" \
+    -t \
+    -c "${LAST_VAL_SQL}" \
+    --set ON_ERROR_ROLLBACK=on \
+    --set ON_ERROR_STOP=off
+    )
+
+echo "SEQUENCE VALUE IN NEW DB BEFORE UPDATE: $SEQ_VAL_BEFORE_UPDATE"
