601dfcc0aa64cd0ca45477028e9e3113288f42bd
Karim Lawani <k7lawani@gmail.com>
2019-08-16 13:02:51 +0200

    trying

--- migration.sh
+++ migration.sh
@@ -5,3 +5,38 @@ set -e
 set -u
 
 echo "* Starting migration..."
+echo
+
+if [ "$#" -ne 10 ]; then
+    echo "* Illegal number of arguments."
+    echo "* Usage: $0 [old_host] [old_port] [old_username] [old_password] [old_db] [referenceType] [new_host] [new_port] [new_username] [new_db]"
+    echo
+    echo "* Exiting"
+    echo
+    exit 1
+fi
+
+OLD_HOST="$1"
+OLD_PORT="$2"
+OLD_USERNAME="$3"
+OLD_PASSWORD="$4"
+OLD_DB="$5"
+REFERENCETYPE="$6"
+NEW_HOST="$7"
+NEW_PORT="$8"
+NEW_USERNAME="$9"
+NEW_DB="${10}"
+
+echo "* OLD DB hostname: $OLD_HOST"
+echo "* OLD DB port: $OLD_PORT"
+echo "* OLD DB username: $OLD_USERNAME"
+echo "* OLD DB password: $OLD_PASSWORD"
+echo "* OLD DB name: $OLD_DB"
+
+echo "* SELECTED REFERENCE TYPE: $REFERENCETYPE"
+
+echo "* NEW DB hostname: $NEW_HOST"
+echo "* NEW DB port: $NEW_PORT"
+echo "* NEW DB username: $NEW_USERNAME"
+echo "* NEW DB name: $NEW_DB"
+echo
